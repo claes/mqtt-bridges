b3828756b627d76ebb9d4a96a30d882fbe0068f7
claes <claes@holmerson.net>
2024-12-18 20:09:26 +0100

    Cleaned up root flake

--- flake.nix
+++ flake.nix
@@ -1,19 +1,11 @@
 {
-  description = "Snapcast-MQTT";
-
-  # Nixpkgs / NixOS version to use.
+  description = "MQTT bridges";
   inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
 
   outputs = {
     self,
     nixpkgs,
   }: let
-    # to work with older version of flakes
-    lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-
-    # Generate a user-friendly version number.
-    version = builtins.substring 0 8 lastModifiedDate;
-
     # System types to support.
     supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
 
@@ -23,39 +15,6 @@
     # Nixpkgs instantiated for supported system types.
     nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
   in {
-    # Provide some binary packages for selected system types.
-    packages = forAllSystems (system: let
-      pkgs = nixpkgsFor.${system};
-    in {
-      snapcast-mqtt = pkgs.buildGoModule {
-        pname = "snapcast-mqtt";
-        inherit version;
-        # In 'nix develop', we don't need a copy of the source tree
-        # in the Nix store.
-
-        # preBuild = ''
-        #   export GOWORK=off
-        # '';
-
-        src = ./.;
-        modRoot = ./snapcast-mqtt;
-        proxyVendor = true;
-
-        # This hash locks the dependencies of this package. It is
-        # necessary because of how Go requires network access to resolve
-        # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-        # details. Normally one can build with a fake sha256 and rely on native Go
-        # mechanisms to tell you what the hash should be or determine what
-        # it should be "out-of-band" with other tooling (eg. gomod2nix).
-        # To begin with it is recommended to set this, but one must
-        # remeber to bump this hash when your dependencies change.
-        #vendorSha256 = pkgs.lib.fakeSha256;
-
-        vendorHash = null;
-      };
-    });
-
-    # Add dependencies that are only needed for development
     devShells = forAllSystems (system: let
       pkgs = nixpkgsFor.${system};
     in {
@@ -73,14 +32,8 @@
           libcec_platform
           pkg-config
           glibc.static
-          #musl
         ];
       };
     });
-
-    # The default package for 'nix build'. This makes sense if the
-    # flake provides only one package or there is a clear "main"
-    # package.
-    defaultPackage = forAllSystems (system: self.packages.${system}.snapcast-mqtt);
   };
 }
