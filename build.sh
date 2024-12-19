a5ebd7354fa32deb6ae9504587bcb07928879bb7
claes <claes@holmerson.net>
2024-12-19 23:09:37 +0100

    Add mpd-mqtt

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # common
-for package in common pulseaudio-mqtt rotel-mqtt routeros-mqtt samsungtv-mqtt snapcast-mqtt cec-mqtt; do
+for package in common mpd-mqtt pulseaudio-mqtt rotel-mqtt routeros-mqtt samsungtv-mqtt snapcast-mqtt cec-mqtt ; do
   go build ./$package/...
 done
